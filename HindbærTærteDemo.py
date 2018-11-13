d93d30e959a7410767ad21fe49d96c6a911a5516
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-13 14:12:05 +0100

    Create HindbærTærteDemo.py
    Lavet i skolen

---
+++ HindbærTærteDemo.py
@@ -0,0 +1,46 @@
+from sense_hat import SenseHat
+from time import sleep
+from random import randint
+
+sense = SenseHat()
+
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255,255,255)
+nothing = (0,0,0)
+pink = (255,105, 180)
+
+def pick_random_colour():
+  random_red = randint(0, 255)
+  random_green = randint(0, 255)
+  random_blue = randint(0, 255)
+  return (random_red, random_green, random_blue)
+  
+#while True:
+  sense.show_message("F", text_colour=nothing, back_colour=pick_random_colour(), scroll_speed=0.03)
+  sense.show_message("R", text_colour=nothing, back_colour=pick_random_colour(), scroll_speed=0.03)
+  sense.show_message("E", text_colour=nothing, back_colour=pick_random_colour(), scroll_speed=0.03)
+  sense.show_message("D", text_colour=nothing, back_colour=pick_random_colour(), scroll_speed=0.03)
+  sense.show_message("E", text_colour=nothing, back_colour=pick_random_colour(), scroll_speed=0.03)
+  sense.show_message("R", text_colour=nothing, back_colour=pick_random_colour(), scroll_speed=0.03)
+  sense.show_message("I", text_colour=nothing, back_colour=pick_random_colour(), scroll_speed=0.03)
+  sense.show_message("K", text_colour=nothing, back_colour=pick_random_colour(), scroll_speed=0.03)
+  
+
+while True:
+  t = sense.get_temperature()
+  p = sense.get_pressure()
+  h = sense.get_humidity()
+  
+  t = round(t, 1)
+  p = round(p, 1)
+  h = round(h, 1)
+  
+  if (t>40):
+    sense.show_message("Puha, det er varmt!",text_colour=red, scroll_speed=0.05)
+  
+  else:
+    sense.show_message(str(t), text_colour=green, scroll_speed=0.05)
+    
