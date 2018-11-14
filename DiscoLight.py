55a66fceab2c0db4146c4d115ab5b1e428ee838a
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-14 10:55:05 +0100

    Create DiscoLight.py
    Awesome discolight

---
+++ DiscoLight.py
@@ -0,0 +1,36 @@
+from sense_hat import SenseHat
+from random import randint
+import time
+
+sense = SenseHat()
+
+count = 0.2
+t = 5
+
+def pick_random_colour():
+  random_red = randint(0, 255)
+  random_green = randint(0, 255)
+  random_blue = randint(0, 255)
+  return (random_red, random_green, random_blue)
+  
+
+  
+while True:
+  o = pick_random_colour()
+  
+  all_pixels = [
+  o,o,o,o,o,o,o,o,
+  o,o,o,o,o,o,o,o,
+  o,o,o,o,o,o,o,o,
+  o,o,o,o,o,o,o,o,
+  o,o,o,o,o,o,o,o,
+  o,o,o,o,o,o,o,o,
+  o,o,o,o,o,o,o,o,
+  o,o,o,o,o,o,o,o
+]
+  sense.set_pixels(all_pixels)
+  time.sleep(t)
+  t = t-count
+  sense.clear()
+
+  
