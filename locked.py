319a72bea31431049ae4ce3c1e345b8bbb0885e1
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-26 14:19:28 +0100

    Create locked.py

---
+++ locked.py
@@ -0,0 +1,23 @@
+from sense_hat import SenseHat
+from random import randint
+import time
+
+sense = SenseHat()
+
+g = (0,255,0)
+r = (255,0,0)
+s = (0,0,0)
+  
+locked = [
+r,r,r,r,r,r,r,r,
+r,r,r,s,s,r,r,r,
+r,r,s,r,r,s,r,r,
+r,s,r,r,r,r,s,r,
+r,s,s,s,s,s,s,r,
+r,s,s,s,s,s,s,r,
+r,s,s,s,s,s,s,r,
+r,r,r,r,r,r,r,r
+]
+
+while True:
+  sense.set_pixels(locked)
