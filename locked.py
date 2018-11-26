b5b63a5839efa098aa0697c5a50ad4fd1c66c8c4
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-26 14:27:48 +0100

    Update locked.py

--- locked.py
+++ locked.py
@@ -7,16 +7,17 @@ sense = SenseHat()
 g = (0,255,0)
 r = (255,0,0)
 s = (0,0,0)
+o = (255,127,80)
   
 locked = [
 r,r,r,r,r,r,r,r,
 r,r,r,s,s,r,r,r,
 r,r,s,r,r,s,r,r,
 r,s,r,r,r,r,s,r,
+r,o,o,o,o,o,o,r,
 r,s,s,s,s,s,s,r,
-r,s,s,s,s,s,s,r,
-r,s,s,s,s,s,s,r,
-r,r,r,r,r,r,r,r
+r,s,s,r,r,s,s,r,
+r,s,s,r,r,s,s,r,
 ]
 
 while True:
