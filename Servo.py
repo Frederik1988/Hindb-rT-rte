92de9d2a9e644b6b1d9d1a9febfb1d085221522e
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-27 12:22:31 +0100

    Update Servo.py

--- Servo.py
+++ Servo.py
@@ -1,20 +1,6 @@
 import RPi.GPIO as GPIO
 import time
 
-def getkey():
-        fd = sys.stdin.fileno()
-        old = termios.tcgetattr(fd)
-        new = termios.tcgetattr(fd)
-        new[3] = new[3] & ~TERMIOS.ICANON & ~TERMIOS.ECHO
-        new[6][TERMIOS.VMIN] = 1
-        new[6][TERMIOS.VTIME] = 0
-        termios.tcsetattr(fd, TERMIOS.TCSANOW, new)
-        c = None
-        try:
-                c = os.read(fd, 1)
-        finally:
-                termios.tcsetattr(fd, TERMIOS.TCSAFLUSH, old)
-        return c
     
 GPIO.setmode (GPIO.BOARD)
 GPIO.setup (11, GPIO.OUT)
@@ -22,11 +8,11 @@ pwm = GPIO.PWM (11, 50)
 pwm.start(12)
 
 while True:
-    key = getkey()
-    if (key == 's'):
+    
+    if (input('l')):
         pvm.ChangeDutyCycle(12)
         
-    if(key=='o'):
+    if(input('o')):
         pwm.ChangeDutyCycle(7)
     
 
