298cb3e4c895365be04a82bc4bcafc075dffbf79
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-27 11:50:35 +0100

    Update Servo.py

--- Servo.py
+++ Servo.py
@@ -3,6 +3,6 @@ import time
 GPIO.setmode (GPIO.BOARD)
 GPIO.setup (11, GPIO.OUT)
 pwm = GPIO.PWM (11, 50)
-pwm.start(9.5)
+pwm.start(15.5)
 time.sleep(5)
-pwm.ChangeDutyCycle(9.5)
+pwm.ChangeDutyCycle(1.5)
