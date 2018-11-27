f1b3ec8d8250e68c952c836f9a059e8765bbe2df
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-27 13:26:46 +0100

    Create ServoClient.py

---
+++ ServoClient.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+import socket
+
+TCP_IP = '192.168.24.188' 
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+data = s.recv(BUFFER_SIZE)
+    
+GPIO.setmode (GPIO.BOARD)
+GPIO.setup (11, GPIO.OUT)
+pwm = GPIO.PWM (11, 50)
+pwm.start(12)
+
+while True:
+ 
+ if (data =='l'):
+  pwm.ChangeDutyCycle(7)
+ if (data == 'o'):
+  pwm.ChangeDutyCycle(12)
+ 
+ if (char == 'q'):
+  break
