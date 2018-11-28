382dd107315e57e95d5af2932c834b5a198e7e81
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-28 09:55:20 +0100

    Update ServoClient.py

--- ServoClient.py
+++ ServoClient.py
@@ -2,9 +2,12 @@ import RPi.GPIO as GPIO
 import time
 import socket
 
-s = socket.socket()
+TCP_IP = "192.168.24.12"
+TCP_PORT = 9576
 
-s.connect(("192.168.24.12", 9576)) 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((TCP_IP, TCP_PORT)) 
 
 data = s.recv(1024)
     
