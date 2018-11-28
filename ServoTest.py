cc0699bddcf6c862513b2b8827a5b9969176650e
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-28 13:09:50 +0100

    Update ServoTest.py

--- ServoTest.py
+++ ServoTest.py
@@ -13,30 +13,30 @@ sock.connect((TCP_IP, TCP_PORT))
 
 sense = SenseHat()
 
-g = (0,255,0)
+g = (0,0,255)
 r = (255,0,0)
 s = (0,0,0)
 
 locked = [
+r,s,s,s,s,s,s,r, 
+r,s,s,s,s,s,s,r,
+r,s,s,s,s,s,s,r,
+r,s,s,s,s,s,s,r,
+r,s,r,r,r,r,s,r,  
+r,r,s,r,r,s,r,r, 
+r,r,r,s,s,r,r,r,    
 r,r,r,r,r,r,r,r,
-r,r,r,s,s,r,r,r,
-r,r,s,r,r,s,r,r,
-r,s,r,r,r,r,s,r,
-r,s,s,s,s,s,s,r,
-r,s,s,s,s,s,s,r,
-r,s,s,s,s,s,s,r,
-r,s,s,s,s,s,s,r,
 ]
 
 unlocked = [
-g,g,g,g,g,g,g,g,
-g,g,g,s,s,g,g,g,
-g,g,s,g,g,s,g,g,
+g,s,s,s,s,s,s,g,
+g,s,s,s,s,s,s,g,
+g,s,s,s,s,s,s,g,
+g,s,s,s,s,s,s,g,
 g,g,g,g,g,g,s,g,
-g,s,s,s,s,s,s,g,
-g,s,s,s,s,s,s,g,
-g,s,s,s,s,s,s,g,
-g,s,s,s,s,s,s,g,
+g,g,s,g,g,s,g,g,
+g,g,g,s,s,g,g,g,
+g,g,g,g,g,g,g,g, 
 ]
 
 GPIO.setmode (GPIO.BOARD)
