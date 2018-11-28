a2cf7749b103c014eff4f389e7c47b4a8da1c7b2
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-28 11:27:51 +0100

    Update Servo.py

--- Servo.py
+++ Servo.py
@@ -1,8 +1,14 @@
 import RPi.GPIO as GPIO
 import time
-
+from sense_hat import SenseHat
 import sys, termios, tty, os, time
- 
+
+sense = SenseHat()
+
+g = (0,255,0)
+r = (255,0,0)
+s = (0,0,0)
+
 def getch():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -16,6 +22,28 @@ def getch():
  
 button_delay = 0.2
 
+locked = [
+r,r,r,r,r,r,r,r,
+r,r,r,s,s,r,r,r,
+r,r,s,r,r,s,r,r,
+r,s,r,r,r,r,s,r,
+r,s,s,s,s,s,s,r,
+r,s,s,s,s,s,s,r,
+r,s,s,s,s,s,s,r,
+r,s,s,s,s,s,s,r,
+]
+
+open = [
+g,g,g,g,g,g,g,g,
+g,g,g,s,s,g,g,g,
+g,g,s,g,g,s,g,g,
+g,g,g,g,g,g,s,g,
+g,s,s,s,s,s,s,g,
+g,s,s,s,s,s,s,g,
+g,s,s,s,s,s,s,g,
+g,s,s,s,s,s,s,g,
+]
+
     
 GPIO.setmode (GPIO.BOARD)
 GPIO.setup (11, GPIO.OUT)
@@ -26,8 +54,10 @@ while True:
  char = getch()
  if (char =='l'):
   pwm.ChangeDutyCycle(7)
+  sense.set_pixels(locked)
  if (char == 'o'):
   pwm.ChangeDutyCycle(12)
+  sense.set_pixels(open)
  
  if (char == 'q'):
   break
