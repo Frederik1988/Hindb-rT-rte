cd28ee07c05c85172299c837c962edc899ac162c
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-28 14:04:28 +0100

    Update ServoTest.py

--- ServoTest.py
+++ ServoTest.py
@@ -59,11 +59,11 @@ while True:
   if (message =='l'):
     pwm.ChangeDutyCycle(7)
     sense.set_pixels(locked)
-    sock.send(messageLocked)
+    sock.sendto(bytes(messageLocked, "UTF-8")
   if (message == 'o'):
     pwm.ChangeDutyCycle(12)
     sense.set_pixels(unlocked)
-    sock.send(messageUnlocked)
+    sock.sendto(bytes(messageUnlocked, "UTF-8")
   if (message == 'q'):
     break
   
