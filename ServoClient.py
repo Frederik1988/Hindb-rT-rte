01f0774ba32ea658c94d99a9d7de570c632fc9aa
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-28 09:27:23 +0100

    Update ServoClient.py

--- ServoClient.py
+++ ServoClient.py
@@ -3,13 +3,13 @@ import time
 import socket
 
 TCP_IP = '127.0.0.1'
-port = 9576
+TCP_PORT = 9576
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print(s)
 
-s.connect((TCP_IP, port))
+s.connect((TCP_IP, TCP_PORT))
 
 data = s.recv(1024)
     
