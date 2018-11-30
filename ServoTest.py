41949374b27e90a74c4e5f6b220cef49a4529972
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-11-30 12:43:29 +0100

    Farver rettet i led lysene

--- ServoTest.py
+++ ServoTest.py
@@ -20,29 +20,29 @@ messageQuit = "Goodbye.."
 sense = SenseHat()
 
 g = (0,255,0)
-r = (0,0,0)
-s = (255,0,0)
+r = (255,0,0)
+s = (0,0,0)
 
 locked = [
-r,s,s,s,s,s,s,r, 
-r,s,s,s,s,s,s,r,
-r,s,s,s,s,s,s,r,
-r,s,s,s,s,s,s,r,
-r,r,s,r,r,s,r,r,  
-r,r,s,r,r,s,r,r, 
-r,r,r,s,s,r,r,r,    
-r,r,r,r,r,r,r,r,
+s,r,r,r,r,r,r,s, 
+s,r,r,r,r,r,r,s,
+s,r,r,r,r,r,r,s,
+s,r,r,r,r,r,r,s,
+s,s,r,s,s,r,s,s,  
+s,s,r,s,s,r,s,s, 
+s,s,s,r,r,s,s,s,    
+s,s,s,s,s,s,s,s,
 ]
 
 unlocked = [
-r,g,g,g,g,g,g,r,
-r,g,g,g,g,g,g,r,
-r,g,g,g,g,g,g,r,
-r,g,g,g,g,g,g,r,
-r,r,r,r,r,g,r,r,
-r,r,g,r,r,g,r,r,
-r,r,r,g,g,r,r,r,
-r,r,r,r,r,r,r,r,
+s,g,g,g,g,g,g,s,
+s,g,g,g,g,g,g,s,
+s,g,g,g,g,g,g,s,
+s,g,g,g,g,g,g,s,
+s,s,s,s,s,g,s,s,
+s,s,g,s,s,g,s,s,
+s,s,s,g,g,s,s,s,
+s,s,s,s,s,s,s,s,
 ]
 
 GPIO.setmode (GPIO.BOARD)
