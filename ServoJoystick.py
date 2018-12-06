4fba723aac7ae278d4e96b97ee5b553b9b0e7af7
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-12-06 13:02:18 +0100

    Update ServoJoystick.py

--- ServoJoystick.py
+++ ServoJoystick.py
@@ -93,7 +93,6 @@ async def joystick(i):
 
 #loop.run_until_complete(recieveMessage())
 
-loop = asyncio.get_event_loop()
-cors = asyncio.wait([joystick(i),recieveMessage()])
-loop.run_until_complete(cors)
-
+loop = asyncio.get_event_loop() 
+cors = asyncio.wait([recieveMessage(),joystick(i)])
+loop.un_until_complete(cors)
