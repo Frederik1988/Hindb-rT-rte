31b0ef3ea9a50ac5a081742c5407c7ed7460e43b
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-12-08 10:20:11 +0100

    Update ServoJoystick.py

--- ServoJoystick.py
+++ ServoJoystick.py
@@ -22,25 +22,25 @@ r = (255,0,0)
 s = (0,0,0)
 
 locked = [
-s,r,r,r,r,r,r,s, 
-s,r,r,r,r,r,r,s,
-s,r,r,r,r,r,r,s,
-s,r,r,r,r,r,r,s,
-s,s,r,s,s,r,s,s,  
-s,s,r,s,s,r,s,s, 
-s,s,s,r,r,s,s,s,    
-s,s,s,s,s,s,s,s,
+  s,s,s,s,s,s,s,s,
+  s,s,s,r,r,s,s,s,
+  s,s,r,s,s,r,s,s,
+  s,s,r,s,s,r,s,s,  
+  s,r,r,r,r,r,r,s,
+  s,r,r,r,r,r,r,s,
+  s,r,r,r,r,r,r,s,
+  s,r,r,r,r,r,r,s
 ]
 
 unlocked = [
-s,g,g,g,g,g,g,s,
-s,g,g,g,g,g,g,s,
-s,g,g,g,g,g,g,s,
-s,g,g,g,g,g,g,s,
-s,s,s,s,s,g,s,s,
-s,s,g,s,s,g,s,s,
-s,s,s,g,g,s,s,s,
-s,s,s,s,s,s,s,s,
+  s,s,s,s,s,s,s,s,
+  s,s,s,g,g,s,s,s,
+  s,s,g,s,s,g,s,s,
+  s,s,s,s,s,g,s,s,
+  s,g,g,g,g,g,g,s,
+  s,g,g,g,g,g,g,s,
+  s,g,g,g,g,g,g,s,
+  s,g,g,g,g,g,g,s
 ]
 
 GPIO.setmode (GPIO.BOARD)
