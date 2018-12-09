9031de68745e1e69721733fbddade9ed0c43550a
Frederik1988 <33230403+Frederik1988@users.noreply.github.com>
2018-12-09 17:34:57 +0100

    Klar til export

---
+++ padlockDisplay.py
@@ -0,0 +1,25 @@
+g = (0,255,0)
+r = (255,0,0)
+s = (0,0,0)
+
+locked = [
+  s,s,s,s,s,s,s,s,
+  s,s,s,r,r,s,s,s,
+  s,s,r,s,s,r,s,s,
+  s,s,r,s,s,r,s,s,  
+  s,r,r,r,r,r,r,s,
+  s,r,r,r,r,r,r,s,
+  s,r,r,r,r,r,r,s,
+  s,r,r,r,r,r,r,s
+]
+
+unlocked = [
+  s,s,s,s,s,s,s,s,
+  s,s,s,g,g,s,s,s,
+  s,s,g,s,s,g,s,s,
+  s,s,s,s,s,g,s,s,
+  s,g,g,g,g,g,g,s,
+  s,g,g,g,g,g,g,s,
+  s,g,g,g,g,g,g,s,
+  s,g,g,g,g,g,g,s
+]
